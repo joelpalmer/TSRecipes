1690bc8be84e9fd4ccd44fb675facc0c817a8cdb
joelpalmer <joel.palmer@demand-energy.com>
2017-03-18 13:49:50 -0500

    Angular directive example

--- app.ts
+++ app.ts
@@ -6,3 +6,31 @@ class HomeController {
     }
 }
 myApp.controller("HomeController", HomeController);
+
+interface IMessageContainerScope extends ng.IScope {
+    message: string;
+}
+
+class MessageContainer {
+    public templateUrl = "messageView.html";
+    public scope = {
+        message: "="
+    };
+    // constructor() {
+
+    // }
+
+    public link: ng.IDirectiveLinkFn = (scope: IMessageContainerScope, element, attrs) => {
+        scope.message = "Joel";
+    }
+
+    public static Factory() {
+        const directive = () => {
+            return new MessageContainer();
+        };
+        directive.$inject = [];
+        return directive;
+    }
+}
+
+myApp.directive('messageContainer', MessageContainer.Factory());
